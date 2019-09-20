dc11ec7c8dc58fa72366aca135669330ebb14156
vehiclefleetmanager <tmm.miernik@gmail.com>
2019-09-20 18:54:29 +0200

    Initial commit

---
+++ src/main/java/com/example/springhomeworktasks/SpringHomeworkTasksApplication.java
@@ -0,0 +1,13 @@
+package com.example.springhomeworktasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHomeworkTasksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHomeworkTasksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springhomeworktasks/SpringHomeworkTasksApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springhomeworktasks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringHomeworkTasksApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
